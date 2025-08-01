ab8aa98710741464d031166b0b31f3ff205f233c
amruthaantony-prog <amruthaantony21@gmail.com>
2025-08-01 15:35:18 +0100

    Update smp.py

--- smp.py
+++ smp.py
@@ -1,10 +1,7 @@
-for item in rs2:
-    # Find all matching entries
-    matched_info = [
-        f"page_start:{start}, page_end:{end}, title:{title}"
-        for start, end, title in page_map
-        if start <= item['page_no'] <= end
-    ]
-    
-    # Join if multiple matches
-    extra_ref = " | ".join(matched_info) if matched_info else ""
+matched_info = []
+    for lst in page_map:
+        start = lst[0]
+        end = lst[-3] if isinstance(lst[-3], int) else lst[0]  # In case second last item isn't numeric
+        if start <= item['page_no'] <= end:
+            info = f"{lst[0]}, {lst[-2]}, {lst[-1]}"
+            matched_info.append(info)
