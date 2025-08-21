de99bae5c0878fd14ccf28324b6b641c9b68f9bc
amruthaantony-prog <amruthaantony21@gmail.com>
2025-08-21 14:24:00 +0100

    Update cla.py

--- cla.py
+++ cla.py
@@ -1,17 +1,20 @@
-import subprocess
+import ghostscript
+import sys
 
 input_pdf = "american.pdf"
 output_pdf = "american_fixed.pdf"
 
-cmd = [
-    "gs",
-    "-o", output_pdf,
+args = [
+    "ps2pdf",   # program name, not used
+    "-dNOPAUSE",
+    "-dBATCH",
     "-sDEVICE=pdfwrite",
     "-dPDFSETTINGS=/prepress",
+    f"-sOutputFile={output_pdf}",
     input_pdf
 ]
 
-# Run the command
-subprocess.run(cmd, check=True)
+# ghostscript expects arguments as bytes
+ghostscript.Ghostscript(*map(str.encode, args))
 
 print(f"Fixed PDF saved as {output_pdf}")
